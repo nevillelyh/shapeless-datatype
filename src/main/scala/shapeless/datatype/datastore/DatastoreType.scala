bc7f46b3e4c7129fc9995ea3c0f1bae9697b570b
Neville Li <neville@spotify.com>
2016-10-18 08:36:48 -0400

    support both Datastore Entity and Entity.Builder

--- src/main/scala/shapeless/datatype/datastore/DatastoreType.scala
+++ src/main/scala/shapeless/datatype/datastore/DatastoreType.scala
@@ -4,12 +4,29 @@ import com.google.datastore.v1.Entity
 import shapeless._
 
 class DatastoreType[A] extends Serializable {
+  def fromEntityBuilder[L <: HList](m: Entity.Builder)
+                                   (implicit
+                                    gen: LabelledGeneric.Aux[A, L],
+                                    fromL: FromEntity[L]): Option[A] =
+    fromL(m).map(gen.from)
+
   def fromEntity[L <: HList](m: Entity)
-                            (implicit gen: LabelledGeneric.Aux[A, L], fromL: FromEntity[L])
-  : Option[A] = fromL(m.toBuilder).map(gen.from)
+                            (implicit
+                             gen: LabelledGeneric.Aux[A, L],
+                             fromL: FromEntity[L]) : Option[A] =
+    fromL(m.toBuilder).map(gen.from)
+
+  def toEntityBuilder[L <: HList](a: A)
+                                 (implicit
+                                  gen: LabelledGeneric.Aux[A, L],
+                                  toL: ToEntity[L]): Entity.Builder =
+    toL(gen.to(a))
+
   def toEntity[L <: HList](a: A)
-                          (implicit gen: LabelledGeneric.Aux[A, L], toL: ToEntity[L])
-  : Entity = toL(gen.to(a)).build()
+                          (implicit
+                           gen: LabelledGeneric.Aux[A, L],
+                           toL: ToEntity[L]): Entity =
+    toL(gen.to(a)).build()
 }
 
 object DatastoreType {
