69a3e3d040124dac9ad02c54423bc50d0fc7f8fb
Neville Li <neville@spotify.com>
2016-10-19 08:57:08 -0400

    simplify SerializableUtils

--- test/src/main/scala/shapeless/datatype/test/SerializableUtils.scala
+++ test/src/main/scala/shapeless/datatype/test/SerializableUtils.scala
@@ -4,29 +4,19 @@ import java.{io => jio}
 
 object SerializableUtils {
 
-  def serializeToByteArray(value: Serializable): Array[Byte] = {
-    try {
-      val buffer = new jio.ByteArrayOutputStream()
-      val oos = new jio.ObjectOutputStream(buffer)
-      oos.writeObject(value)
-      buffer.toByteArray
-    } catch {
-      case e: jio.IOException =>
-        throw new IllegalArgumentException(s"unable to serialize $value", e)
-    }
+  private def serializeToByteArray(value: Serializable): Array[Byte] = {
+    val buffer = new jio.ByteArrayOutputStream()
+    val oos = new jio.ObjectOutputStream(buffer)
+    oos.writeObject(value)
+    buffer.toByteArray
   }
 
-  def deserializeFromByteArray(encodedValue: Array[Byte], description: String): AnyRef = {
-    try {
-      val ois = new jio.ObjectInputStream(new jio.ByteArrayInputStream(encodedValue))
-      ois.readObject()
-    } catch {
-      case e @ (_: jio.IOException | _: ClassNotFoundException) =>
-        throw new IllegalArgumentException(s"unable to deserialize $description", e)
-    }
+  private def deserializeFromByteArray(encodedValue: Array[Byte]): AnyRef = {
+    val ois = new jio.ObjectInputStream(new jio.ByteArrayInputStream(encodedValue))
+    ois.readObject()
   }
 
   def ensureSerializable[T <: Serializable](value: T): T =
-    deserializeFromByteArray(serializeToByteArray(value), value.toString).asInstanceOf[T]
+    deserializeFromByteArray(serializeToByteArray(value)).asInstanceOf[T]
 
 }
